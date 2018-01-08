5f1e2aaf7b56cee82a66e0e9b60dae815795dd2f
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-08 16:49:50 +0530

    Update and rename README.md to odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the value is ");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the value is even");
+}
+else
+{
+printf("the value is odd");
+}
+getch();
+}
