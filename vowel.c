8bda1ad79d2281439720cf0af06607d9a021ef56
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-08 16:59:53 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char leter;
+printf("the character is":):
+scanf("%d",&letter);
+if(letter==a||letter==e||letter==i||letter==o||letter==u)
+{
+printf("vowel");
+}
+else
+{
+printf("constant");
+}
+getch();
+}
