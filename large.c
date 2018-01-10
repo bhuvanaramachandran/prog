6ee05851b2b7de610de73fc7038ee3766161b4b7
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-10 16:17:33 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1=3,n2=1,n3=1;
+clrscr();
+if(n1>n2&&n1>n3)
+{
+printf("n1 is large");
+}
+elseif(n2>n1&&n2>n3)
+{
+printf("n2 is large");
+}
+else
+{
+printf("n3 is large);
+}
+getch();
+}
