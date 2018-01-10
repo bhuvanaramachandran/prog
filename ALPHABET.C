b76486ff0260a2c7a3eb31f47df91b52e0d8bed9
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-10 15:52:54 +0530

    Update ALPHABET.C

--- ALPHABET.C
+++ ALPHABET.C
@@ -1,1 +1,17 @@
-#
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char letter;
+  printf("enter the value of letter");
+  scanf("%c",&letter);
+  if(a<letter<z||A<letter<Z)
+  {
+    printf("alphabet");
+  }
+  else
+  {
+    printf("number");
+  }
+getch();
+}
