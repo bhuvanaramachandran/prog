f7fdb45f578a068b8f87b400379c7856e6dc7de1
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-19 21:52:24 +0530

    Create 2numbers.c

---
+++ 2numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>    
+void main(){
+
+int a,b,c;
+
+clrscr();
+
+printf("enter first number");
+
+scanf("%d",&a);
+
+printf("enter second number");
+
+scanf("%d",&b);
+
+c=a+b;
+
+printf("total number is %d",c);
+
+}
