5c0bf8a0fe410fb46f72de534846914ff8b3546a
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-21 10:17:51 +0530

    Create oddin2.c

---
+++ oddin2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the number");
+scanf(" %d",&b");
+printf("enter upto 1 to %d",n);
+for(a=0;a<b;a=a+b)
+{
+printf("%d \n",a);
+}
+getch();
+}
