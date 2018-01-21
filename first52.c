7854155ea2ec3cfb63a92d365e4a7a0f128243e4
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-21 11:34:01 +0530

    Create first52.c

---
+++ first52.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int marks[5]={10,20,30,40,50};
+int i=0;
+counter =0;
+printf(" enter the number less than 5");
+scanf("%d",&counter);
+for(i=0;i<<count;i++)
+{
+printf(" %d\n",mark[i]);
+}
+}
