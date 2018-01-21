afe673870cf57c660157c7180eb2ae3ae3fd561e
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-21 11:13:46 +0530

    Create factorial2.c

---
+++ factorial2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int add(intn);
+voidmain()
+{
+int n;
+printf("enter the positive number");
+scanf(" %d",&n);
+printf("sum=%d",addnumber);
+}
+int add(int n)
+{
+if(n!=0)
+{
+return n+number(n-1);
+}
+else
+{
+return n;
+}
+getch();
+}
