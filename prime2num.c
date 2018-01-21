9b747049e493d7ceda7f5a6b3bf294e2bf12a7db
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-21 10:57:12 +0530

    Create prime2num.c

---
+++ prime2num.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int low,high,i,temp;
+printf(" enter the two number");
+scanf(" %d %d",&low,&high);
+if(high<low){
+temp=low;
+low=high;
+high=temp;
+}
+printf(" %d %d",low,high);
+while(low<high)
+{
+f=0;
+break;
+}
+}
+if(f==0)
+{
+printf("%d",low);
+++low;
+}
+}
