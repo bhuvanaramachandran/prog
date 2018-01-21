e4424ae7e0d29f8ed41b51e30399ab5d6b7b21e8
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-21 12:16:06 +0530

    Create maximum33.c

---
+++ maximum33.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int [] array={a,b,c,d,e,f};
+printf("the content of the array are:");
+for(int i=0;i<10;i++)
+{
+printf("\t \t \t array["+i+"]="+array[i]);
+}
+getch();
+}
+
