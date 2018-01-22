5553adbc133aa01f430abc752350f5c48442f97b
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-23 00:54:21 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[1000],line=0,i;
+gets(s);
+for(i=0;s[i]!=NULL;i++)
+{
+if(s(i)=='.'||s[i]=='\n'||s[j]=='\t';
+line++;
+}
+printf("%d",line+1);
+return 0;
+}
