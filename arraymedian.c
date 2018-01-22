1b09c2db1bd85c4e254b4a41187a5ba145cdf645
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-22 23:41:46 +0530

    Create arraymedian.c

---
+++ arraymedian.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int middle;
+double avereage,median;
+middle=size/2.0;
+if(size%2)
+{
+Median=(array[middle]+array[middle+1])/2.0;
+cout<<"The median is:"<<average<<ebd 1;
+}
+else
+{
+median=array[middle+0]/1.0;
+}
+return median;
+}
