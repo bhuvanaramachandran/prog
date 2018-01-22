9a672494a80e6313ed16e9a2ca86e355b10a9a82
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-22 23:16:02 +0530

    Create noofwrds.c

---
+++ noofwrds.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[200];
+int count=0;
+printf("Enter the string\n");
+scanf("%[^\n]s",s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]==' ')
+count++;
+}
+printf("Number of words in given string are:%d\n",count+1);
+}
