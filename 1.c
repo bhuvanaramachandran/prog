eb7b351761350feea58e3f4631d8f8944a3fc9bc
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-23 23:32:49 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,s,k;
+scanf("%d",&s);
+int a[20];
+for(i=0;i<s;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<s;i++)
+{
+for(k=i+1;k<s;k++)
+{
+if(a[i]==a[k])
+printf("%d",a[i]);
+}
+return o;
+}
