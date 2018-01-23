52ab26fd79c561ea501278279f74f3d51c9502aa
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-23 19:54:02 +0530

    Create return.c

---
+++ return.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int K,arr[50],s,no;
+scanf("%d",&no);
+for(K=0;K<no;k++);
+{
+scanf("%d",&array[K]);
+}
+for(K=0;k<no;k++)
+{
+for(s=k+1;s<no;s++)
+{
+if(arr[k]==arr[s])
+{
+printf("%d\n",arr[k]);
+}
+}
+}
+getch();
+}
