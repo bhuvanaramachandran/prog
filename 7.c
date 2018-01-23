b3d9b80a68c3e9b07e42a28e698962e3f4f3829e
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-01-23 23:12:23 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,a[50],t,n,flag;
+for(t=0;t<n;t++);
+{
+flag=0;
+for(j=0;j<n;j++)
+{
+if(a[t]==a[j])
+{
+flag++;
+}
+else
+continue:
+}
+if(flag==1)
+printf("%d",a[t]);
+}
+return 0;
+}
